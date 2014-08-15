e7fef26ca24c2ec7d797cd24acd9bdf627705e53
morrillo <gustavo.orrillo@moldeo.coop>
2014-08-15 13:20:29 +0000

    Modified version number

--- __openerp__.py
+++ __openerp__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'eLote Recommendations',
-    'version': '0.1',
+    'version': '0.1.1',
     'category': 'E-Lote',
     'description': "",
     'author': 'Moldeo Interactive',
